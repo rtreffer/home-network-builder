eec7734c402d3bbda1fc8ee6e294b9b58d2d3c8c
Rene Treffer <treffer@measite.de>
2019-04-11 21:59:50 +0200

    Add support for version specific packages

--- openwrt.mk
+++ openwrt.mk
@@ -1,7 +1,7 @@
 BASE_FOLDER ?=$(abspath $(dir $(firstword $(MAKEFILE_LIST))))
 OPENWRT_BUILDER_FOLDER ?=$(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 OPENWRT_DOWNLOAD_FOLDER ?=$(OPENWRT_BUILDER_FOLDER)/tmp/
-OPENWRT_BUILDSCRIPT="set -eux -o pipefail ; mkdir -p /srv/overlay ; rsync -KraulSpalt /srv/overlay-base/* /srv/overlay/ ; rsync -KraulSpalt /srv/overlay-node/* /srv/overlay/ ; cd /srv/builder/ && /usr/local/bin/secret-env /srv/secrets/env.enc /srv/overlay/etc/secrets/env make image PROFILE=\$${PROFILE} EXTRA_IMAGE_NAME=\$${NODE} BIN_DIR=/srv/target/ FILES=/srv/overlay/ PACKAGES=\"\$${BASE_PACKAGES} \$${PACKAGES}\" ; chown \$${TARGET_UID} /srv/target/*\$${NODE}*"
+OPENWRT_BUILDSCRIPT="set -eux -o pipefail ; mkdir -p /srv/overlay ; rsync -KraulSpalt /srv/overlay-base/* /srv/overlay/ ; rsync -KraulSpalt /srv/overlay-node/* /srv/overlay/ ; cd /srv/builder/ && /usr/local/bin/secret-env /srv/secrets/env.enc /srv/overlay/etc/secrets/env make image PROFILE=\$${PROFILE} EXTRA_IMAGE_NAME=\$${NODE} BIN_DIR=/srv/target/ FILES=/srv/overlay/ PACKAGES=\"\$${BASE_PACKAGES} \$${BASE_PACKAGES-VERSION} \$${PACKAGES} \$${PACKAGES_VERSION}\" ; chown \$${TARGET_UID} /srv/target/*\$${NODE}*"
 
 $(OPENWRT_DOWNLOAD_FOLDER)/%:
 	mkdir -p "$@"
@@ -32,7 +32,9 @@ $(1): container-$(2)-$(3)-$(4) $(OPENWRT_DOWNLOAD_FOLDER)/$(2)-$(3)-$(4)
 		-v $(BASE_FOLDER)/target:/srv/target \
 		-v $(BASE_FOLDER)/secrets:/srv/secrets \
 		--env-file $(BASE_FOLDER)/openwrt-all/env \
+		--env-file $(BASE_FOLDER)/openwrt-all/env-$(2) \
 		--env-file $(BASE_FOLDER)/openwrt-$(1)/env \
+		--env-file $(BASE_FOLDER)/openwrt-$(1)/env-$(2) \
 		-e PROFILE=$(5) \
 		-v $(BASE_FOLDER)/openwrt-all/overlay:/srv/overlay-base \
 		-v $(BASE_FOLDER)/openwrt-$(1)/overlay:/srv/overlay-node \
